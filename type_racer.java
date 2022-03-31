d4e8988aee65135ee3f096f14b3e28822ba85f99
Jaden Nguyen <102393572+jaden-nguyen@users.noreply.github.com>
2022-03-31 02:00:39 -0700

    Update type_racer.java

--- type_racer.java
+++ type_racer.java
@@ -4,12 +4,20 @@ import java.util.*;
 
 public class type_racer {
 
+    private static Random rand;
+    private static int randomInt;
     static String[] words = {"mouse", "princess", "coffin", "license", "elephant", 
                         "damage", "mutter", "craftsman", "spine", 
                         "biscuit", "proclaim", "grateful", "anniversary", "competition",
                         "feminine", "banana", "rehabilitation", "concession"};
     public static void main(String[] args) throws InterruptedException {
+        startRace();
         
+    }
+    
+    // prints a randomized set of words to allow 
+    // the user to input the set of words
+    public static void startRace() throws InterruptedException {
         System.out.println("3");
         TimeUnit.SECONDS.sleep(1);
         System.out.println("2");
@@ -17,28 +25,32 @@ public class type_racer {
         System.out.println("1");
         TimeUnit.SECONDS.sleep(1);
 
-        Random rand = new Random();
-
-        int randomInt = 0;
+        rand = new Random();
+        
         for (int i = 0; i <= 10; i++) {
             randomInt = rand.nextInt(18);
             System.out.print(words[randomInt] + " ");
         }
+
         System.out.println();
 
         double startTime = LocalTime.now().toNanoOfDay();
-        
         Scanner console = new Scanner(System.in);
         String typedWords = console.nextLine();
-        
         double endTime = LocalTime.now().toNanoOfDay();
 
+        int WPM = calculateWPM(typedWords, startTime, endTime);
+
+        System.out.println("WPM: " + WPM);
+    }
+
+    // determines the words per minute result 
+    // based on time started and time ended
+    public static int calculateWPM(String typedWords, double startTime, double endTime) {
         double elapsedTime = endTime - startTime;
         double seconds = elapsedTime / 1000000000.0;
 
         int Chars = typedWords.length();
-        int WPM = (int) ((((double)Chars / 5) / seconds) * 60);
-        System.out.println("WPM: " + WPM);
-        
+        return (int) ((((double)Chars / 5) / seconds) * 60);
     }
 }
