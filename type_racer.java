0a727b0987cd84d32895ecb1f0033b1349ba1e8a
Jaden Nguyen <102393572+jaden-nguyen@users.noreply.github.com>
2022-03-31 02:02:22 -0700

    Update type_racer.java

--- type_racer.java
+++ type_racer.java
@@ -7,9 +7,11 @@ public class type_racer {
     private static Random rand;
     private static int randomInt;
     static String[] words = {"mouse", "princess", "coffin", "license", "elephant", 
-                        "damage", "mutter", "craftsman", "spine", 
-                        "biscuit", "proclaim", "grateful", "anniversary", "competition",
-                        "feminine", "banana", "rehabilitation", "concession"};
+                             "damage", "mutter", "craftsman", "spine", 
+                             "biscuit", "proclaim", "grateful", "anniversary", "competition",
+                             "feminine", "banana", "rehabilitation", "concession"
+                             };
+    
     public static void main(String[] args) throws InterruptedException {
         startRace();
         
@@ -24,23 +26,17 @@ public class type_racer {
         TimeUnit.SECONDS.sleep(1);
         System.out.println("1");
         TimeUnit.SECONDS.sleep(1);
-
         rand = new Random();
-        
         for (int i = 0; i <= 10; i++) {
             randomInt = rand.nextInt(18);
             System.out.print(words[randomInt] + " ");
         }
-
         System.out.println();
-
         double startTime = LocalTime.now().toNanoOfDay();
         Scanner console = new Scanner(System.in);
         String typedWords = console.nextLine();
         double endTime = LocalTime.now().toNanoOfDay();
-
         int WPM = calculateWPM(typedWords, startTime, endTime);
-
         System.out.println("WPM: " + WPM);
     }
 
